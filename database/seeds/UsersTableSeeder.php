ca011bd7d8f2e0d5074a1ff787dedc210e812b00
hawkup <spiritsquare@gmail.com>
2017-12-17 13:41:21 +0700

    Added migrate and seed user - add api_token column - seed user for test

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'user',
+            'email' => 'user@localhost',
+            'password' => bcrypt('secret'),
+            'api_token' => 'token'
+        ]);
+    }
+}
